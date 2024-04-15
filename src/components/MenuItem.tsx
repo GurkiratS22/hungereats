83c9c772d70a6cf9b686b9e1e47e7d7886e7f2aa
GurkiratS22 <143457348+GurkiratS22@users.noreply.github.com>
2024-04-14 19:06:45 -0700

    Adding hover on menu cards

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,4 +1,5 @@
 import { MenuItem as MenuItemType } from "../types";
+import { useState, useEffect } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 type Props = {
@@ -7,8 +8,28 @@ type Props = {
 };
 
 const MenuItem = ({ menuItem, addToCart }: Props) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+    addToCart();
+  };
+
+  useEffect(() => {
+    let timer: string | number | NodeJS.Timeout | undefined;
+    if (isClicked) {
+      timer = setTimeout(() => {
+        setIsClicked(false);
+      }, 300); // Adjust the delay as needed
+    }
+    return () => clearTimeout(timer);
+  }, [isClicked]);
+
   return (
-    <Card className="cursor-pointer" onClick={addToCart}>
+    <Card
+      className={`cursor-pointer transition-all duration-300 ease-in-out transform ${isClicked ? 'scale-95' : 'hover:shadow-lg hover:bg-gray-100 hover:scale-105'}`}
+      onClick={handleClick}
+    >
       <CardHeader>
         <CardTitle>{menuItem.name}</CardTitle>
       </CardHeader>
