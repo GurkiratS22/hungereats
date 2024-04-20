dc6f944afec6098c16135a96df61b324b68625ef
GurkiratS22 <143457348+GurkiratS22@users.noreply.github.com>
2024-04-19 20:51:38 -0700

    Minor changes

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -18,7 +18,7 @@ const MainNav = () => {
       ) : (
         <Button
           variant="ghost"
-          className="font-bold hover:text-orange-500 hover:bg-white"
+          className=" text-2xl font-bold hover:text-orange-500 hover:bg-white"
           onClick={async () => await loginWithRedirect()}
         >
           Log In
